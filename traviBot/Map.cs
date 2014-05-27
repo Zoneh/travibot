a4a2986e8bace9f5a25c517d5244aaa62f735f6c
Zoneh <zoneh.eve@gmail.com>
2014-05-27 21:29:53 +0200

    capa

---
+++ traviBot/Map.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using Selenium;
+using OpenQA.Selenium;
+
+
+namespace traviBot
+{
+    class Map
+    {
+        Village village;
+        IWebDriver driver;
+
+        public string animalFinder(int distance, Village village, string animal)
+        {
+            this.village = village;
+            this.driver = Account.driver;
+
+            string tmp = "";
+
+
+            for (int y = (int.Parse(this.village.y) - distance); y < (int.Parse(this.village.y) + distance); y++)
+            {
+                for (int x = (int.Parse(this.village.x) - distance); x < (int.Parse(this.village.x) + distance); x++)
+                {
+                    this.driver.Navigate().GoToUrl("https://ts3.travian.si/position_details.php?x=" + x + "&y=" + y);
+                    string[] str = this.driver.FindElement(By.ClassName("contentContainer")).Text.Split('\n');
+                    string fullstring = this.driver.FindElement(By.ClassName("contentContainer")).Text;
+
+                    if (str[2].Contains("Ropaj nezasedeno oazo"))
+                    {
+                        switch (animal)
+                        {
+                            case "Medved":
+                                if (fullstring.Contains("Medvedov"))
+                                {
+                                    tmp += str[0];
+                                }
+                                break;
+                            case "Krokodil":
+                                if (fullstring.Contains("Krokodilov"))
+                                {
+                                    tmp += str[0];
+                                }
+                                break;
+                            case "Tiger":
+                                if (fullstring.Contains("Tigrov"))
+                                {
+                                    tmp += str[0];
+                                }
+                                break;
+                            case "Slon":
+                                if (fullstring.Contains("Slonov"))
+                                {
+                                    tmp += str[0];
+                                }
+                                break;
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(120);
+                }
+            }
+
+            return tmp;
+        }
+    }
+}
---
+++ traviBot/MapWIndow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace traviBot.Content
+{
+    /// <summary>
+    /// Interaction logic for MapWIndow.xaml
+    /// </summary>
+    public partial class MapWIndow : UserControl
+    {
+        public MapWIndow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Map elephants= new Map();
+            richTextBox1.AppendText(elephants.animalFinder(int.Parse(comboBoxDistance.Text), new Village("","","-51","33"), comboBoxAnimals.Text));
+        }
+    }
+}
